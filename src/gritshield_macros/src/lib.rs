ad29458f899d8b12fc1615c95ff80ffbda9669b0
Ahmed Hassan <digiator2020@gmail.com>
2026-05-17 02:47:32 +0300

    refactor: dry method on macros

--- src/gritshield_macros/src/lib.rs
+++ src/gritshield_macros/src/lib.rs
@@ -3,151 +3,44 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemFn, LitStr};
 
-// --- GET MACRO ---
-#[proc_macro_attribute]
-pub fn get(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(attr as LitStr);
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let fn_name = &input_fn.sig.ident;
-    let vis = &input_fn.vis;
+/// Internal declarative macro helper to generate the repetitive routing attribute pipelines.
+macro_rules! generate_route_macro {
+    ($macro_name:ident, $http_method:ident) => {
+        #[proc_macro_attribute]
+        pub fn $macro_name(attr: TokenStream, item: TokenStream) -> TokenStream {
+            let path = parse_macro_input!(attr as LitStr);
+            let input_fn = parse_macro_input!(item as ItemFn);
+            let fn_name = &input_fn.sig.ident;
+            let vis = &input_fn.vis;
 
-    let wrapper_name = syn::Ident::new(&format!("{}_wrapper", fn_name), fn_name.span());
+            // Generate a unique wrapper identifier matching your 'Handler' type signature
+            let wrapper_name = syn::Ident::new(&format!("{}_wrapper", fn_name), fn_name.span());
 
-    let expanded = quote! {
-        // The original async function from the developer
-        #input_fn
+            let expanded = quote! {
+                // Keep the original developer async function intact in the AST
+                #input_fn
 
-        // The wrapper that matches the 'Handler' type signature
-        #vis fn #wrapper_name(ctx: gritshield::routing::trie::RequestContext) -> gritshield::futures::future::BoxFuture<'static, gritshield::protocol::response::Response> {
-        Box::pin(#fn_name(ctx))
-    }
+                // Define the pin wrapper that converts regular async fn into static BoxFutures
+                #vis fn #wrapper_name(ctx: gritshield::routing::trie::RequestContext) -> gritshield::futures::future::BoxFuture<'static, gritshield::protocol::response::Response> {
+                    Box::pin(#fn_name(ctx))
+                }
 
-        gritshield::inventory::submit! {
-            gritshield::routing::trie::AutoRoute {
-                path: #path,
-                method: gritshield::protocol::request::HttpMethod::GET,
-                handler: #wrapper_name // Register the wrapper
-            }
+                // Automatically submit the route metadata into the compile-time inventory registry
+                gritshield::inventory::submit! {
+                    gritshield::routing::trie::AutoRoute {
+                        path: #path,
+                        method: gritshield::protocol::request::HttpMethod::$http_method,
+                        handler: #wrapper_name
+                    }
+                }
+            };
+            TokenStream::from(expanded)
         }
     };
-    TokenStream::from(expanded)
 }
 
-// --- POST MACRO ---
-#[proc_macro_attribute]
-pub fn post(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(attr as LitStr);
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let fn_name = &input_fn.sig.ident;
-
-    let vis = &input_fn.vis;
-
-    let wrapper_name = syn::Ident::new(&format!("{}_wrapper", fn_name), fn_name.span());
-
-    let expanded = quote! {
-        #input_fn
-
-                // The wrapper that matches the 'Handler' type signature
-        #vis fn #wrapper_name(ctx: gritshield::routing::trie::RequestContext) -> gritshield::futures::future::BoxFuture<'static, gritshield::protocol::response::Response> {
-        Box::pin(#fn_name(ctx))
-    }
-
-        gritshield::inventory::submit! {
-            gritshield::routing::trie::AutoRoute {
-                path: #path,
-                method: gritshield::protocol::request::HttpMethod::POST,
-                handler: #wrapper_name // Register the wrapper
-            }
-        }
-    };
-    TokenStream::from(expanded)
-}
-
-// --- PUT MACRO ---
-#[proc_macro_attribute]
-pub fn put(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(attr as LitStr);
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let fn_name = &input_fn.sig.ident;
-
-    let vis = &input_fn.vis;
-
-    let wrapper_name = syn::Ident::new(&format!("{}_wrapper", fn_name), fn_name.span());
-
-    let expanded = quote! {
-        #input_fn
-
-                // The wrapper that matches the 'Handler' type signature
-        #vis fn #wrapper_name(ctx: gritshield::routing::trie::RequestContext) -> gritshield::futures::future::BoxFuture<'static, gritshield::protocol::response::Response> {
-        Box::pin(#fn_name(ctx))
-    }
-        gritshield::inventory::submit! {
-            gritshield::routing::trie::AutoRoute {
-                path: #path,
-                method: gritshield::protocol::request::HttpMethod::PUT,
-                handler: #wrapper_name // Register the wrapper
-            }
-        }
-    };
-    TokenStream::from(expanded)
-}
-
-// --- PATCH MACRO ---
-#[proc_macro_attribute]
-pub fn patch(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(attr as LitStr);
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let fn_name = &input_fn.sig.ident;
-
-    let vis = &input_fn.vis;
-
-    let wrapper_name = syn::Ident::new(&format!("{}_wrapper", fn_name), fn_name.span());
-
-    let expanded = quote! {
-        #input_fn
-
-                // The wrapper that matches the 'Handler' type signature
-        #vis fn #wrapper_name(ctx: gritshield::routing::trie::RequestContext) -> gritshield::futures::future::BoxFuture<'static, gritshield::protocol::response::Response> {
-        Box::pin(#fn_name(ctx))
-    }
-
-        gritshield::inventory::submit! {
-            gritshield::routing::trie::AutoRoute {
-                path: #path,
-                method: gritshield::protocol::request::HttpMethod::PATCH,
-                handler: #wrapper_name // Register the wrapper
-            }
-        }
-    };
-    TokenStream::from(expanded)
-}
-
-// --- DELETE MACRO ---
-#[proc_macro_attribute]
-pub fn delete(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(attr as LitStr);
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let fn_name = &input_fn.sig.ident;
-
-    let vis = &input_fn.vis;
-
-    let wrapper_name = syn::Ident::new(&format!("{}_wrapper", fn_name), fn_name.span());
-
-    let expanded = quote! {
-        #input_fn
-
-                // The wrapper that matches the 'Handler' type signature
-        #vis fn #wrapper_name(ctx: gritshield::routing::trie::RequestContext) -> gritshield::futures::future::BoxFuture<'static, gritshield::protocol::response::Response> {
-        Box::pin(#fn_name(ctx))
-    }
-
-        gritshield::inventory::submit! {
-            gritshield::routing::trie::AutoRoute {
-                path: #path,
-                method: gritshield::protocol::request::HttpMethod::DELETE,
-                handler: #wrapper_name // Register the wrapper
-            }
-        }
-    };
-    TokenStream::from(expanded)
-}
+generate_route_macro!(get, GET); //
+generate_route_macro!(post, POST); //
+generate_route_macro!(put, PUT); //
+generate_route_macro!(patch, PATCH); //
+generate_route_macro!(delete, DELETE); //
