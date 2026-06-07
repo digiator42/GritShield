d1b8171a1d2b5c70c1a6d83147139fa2b4c379e0
Ahmed Hassan <digiator2020@gmail.com>
2026-06-07 19:04:03 +0300

    update book

--- docs/book/searcher-09f2665d.js
+++ docs/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c577c29e.js',
+                path_to_root + 'searchindex-19b19930.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
