071908394cea8bcb34a8f6fe4bf832e036045ff6
Ahmed Hassan <digiator2020@gmail.com>
2026-06-22 01:47:00 +0300

    feat: async render!

--- src/render.rs
+++ src/render.rs
@@ -1,10 +1,11 @@
-
 #[macro_export]
 macro_rules! render {
-    // Mode A: Standard Maud Markup Render
+    // Mode A: Standard Maud Markup Render (Async)
     ($ctx:expr, $title:expr, $markup:expr) => {{
-        // Pass a reference to the context into main_layout
-        let final_html = crate::root::layout::main_layout($title, $markup, &$ctx).into_string();
+        // Await the main_layout async function
+        let final_html = crate::root::layout::main_layout($title, $markup, &$ctx)
+            .await
+            .into_string();
 
         $crate::protocol::response::Response::new(
             200,
@@ -15,7 +16,9 @@ macro_rules! render {
     // Mode B: Raw HTML String Injection via explicit token flag
     ($ctx:expr, $title:expr, raw $html_string:expr) => {{
         let raw_wrapper = maud::PreEscaped($html_string);
-        let final_html = crate::root::layout::main_layout($title, raw_wrapper, &$ctx).into_string();
+        let final_html = crate::root::layout::main_layout($title, raw_wrapper, &$ctx)
+            .await
+            .into_string();
 
         $crate::protocol::response::Response::new(
             200,
